9ed170fbf6e934f5d4d228cf4d116ec3f48521fa
jmelara001-collab <jmelara001@gmail.com>
2025-12-21 18:07:40 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int LED = 2;
+
+void setup() 
+{
+
+  pinMode(LED, OUTPUT);  
+
+}
+
+void loop() 
+{
+
+  digitalWrite(LED, HIGH);
+  delay(2000);
+  digitalWrite(LED, LOW);
+  delay(1000);  
+
+}
