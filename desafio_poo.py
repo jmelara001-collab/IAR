3e12710e124657c9b98945928342e5abc991dc94
jmelara001-collab <jmelara001@gmail.com>
2026-07-11 19:55:25 -0600

    Completar desafio con SuscripcionPremium y Polimorfismo

--- desafio_poo.py
+++ desafio_poo.py
@@ -14,3 +14,27 @@ class Suscripcion:
 
     def reproducir_contenido(self):
         print(f"El usuario {self.usuario} está viendo contenido estándar.")
+
+# 2. La Clase Hija con Herencia
+class SuscripcionPremium(Suscripcion):
+    def __init__(self, usuario, precio_base, codigo_tarjeta, calidad_video):
+        # Usamos super() en la primera línea para heredar del padre
+        super().__init__(usuario, precio_base, codigo_tarjeta)
+        self.calidad_video = calidad_video
+
+    # 3. Aplicar Polimorfismo (Reescribir el método)
+    def reproducir_contenido(self):
+        print(f"El usuario {self.usuario} está viendo contenido en máxima definición {self.calidad_video}.")
+
+
+# --- Bloque de Pruebas en consola ---
+if __name__ == "__main__":
+    print("--- Probando Suscripción Estándar ---")
+    sub_estandar = Suscripcion("Jordy", 7.99, "1234567890123456")
+    sub_estandar.reproducir_contenido()
+    print(f"Tarjeta: {sub_estandar.obtener_tarjeta_segura()}")
+
+    print("\n--- Probando Suscripción Premium ---")
+    sub_premium = SuscripcionPremium("Jihyo", 15.99, "9876543210987654", "4K")
+    sub_premium.reproducir_contenido()
+    print(f"Tarjeta: {sub_premium.obtener_tarjeta_segura()}")
